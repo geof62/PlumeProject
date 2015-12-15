e4408518818e59061cd6fdc740c43b5fe2f6ac1a
geoffrey <geoffrey.davril@live.fr>
2015-12-15 17:03:57 +0100

    correct

--- framework/Router/models/Router.php
+++ framework/Router/models/Router.php
@@ -16,7 +16,7 @@ class Router
     public function __construct(Request $request, Config $config)
     {
         $this->setUrl($request->getUri());
-        $this->setDataRoutes(include($config ->getConfig('router/fileRoutes')));
+        $this->setDataRoutes(include($config ->getConfig('Router/fileRoutes')));
     }
 
     protected function setUrl(string $uri):self
