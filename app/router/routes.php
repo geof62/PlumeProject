af5abff636f19959a562d478ddf8a14884d357b3
geoffrey <geoffrey.davril@live.fr>
2015-12-16 14:50:36 +0100

    edti

--- app/router/routes.php
+++ app/router/routes.php
@@ -10,7 +10,7 @@ use framework\Router\models\RouteCollection;
 $routes = function(){
     $r = new RouteCollection(function() {return ([
         new Route("index", [], ['ctrl' => 'Global', 'action' => 'index']),
-        new Route("index/{id}", ['id' => '[0-9]{2, 4}'], ['ctrl' => 'Global', 'action' => 'index2'])
+        new Route("index/{id}", ['id' => '[0-9]{2,4}'], ['ctrl' => 'Global', 'action' => 'index2'])
         // tester les méthodes
     ]);
     });
