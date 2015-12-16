a157d02ac971f6b17d295ef7e97f5a9e9758afdb
geoffrey <geoffrey.davril@live.fr>
2015-12-16 13:24:49 +0100

    test

--- app/router/routes.php
+++ app/router/routes.php
@@ -9,7 +9,9 @@ use framework\Router\models\RouteCollection;
 
 $routes = function(){
     $r = new RouteCollection(function() {return ([
-        new Route("index", [], ['ctrl' => 'Global', 'action' => 'index'])
+        new Route("index", [], ['ctrl' => 'Global', 'action' => 'index']),
+        new Route("index/{id}", ['id' => '[0-9]{2, 4}'], ['ctrl' => 'Global', 'action' => 'index2'])
+        // tester les méthodes
     ]);
     });
     return ($r);
